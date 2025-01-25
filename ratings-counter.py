883949fdb8b18e82bfda58fa1668a6441f6152a2
BRPersonal <balajirengan@gmail.com>
2025-01-25 12:45:15 +0530

    minor changes

--- ratings-counter.py
+++ ratings-counter.py
@@ -4,10 +4,12 @@ import collections
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("file:/Users/adiyen/poc/udemy-courses/sparkCourse/ml-100k/u.data")
+lines = sc.textFile("file:///Users/adiyen/poc/udemy-courses/sparkCourse/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
+
+print(f"Total items:{lines.count()}")
