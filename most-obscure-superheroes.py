2b2fcbd4ec7c5f79aa25f8394b763a31cdc22e90
BRPersonal <balajirengan@gmail.com>
2025-01-31 18:11:55 +0530

    added code

--- most-obscure-superheroes.py
+++ most-obscure-superheroes.py
@@ -15,11 +15,13 @@ lines = spark.read.text("file:///Users/adiyen/poc/udemy-courses/sparkCourse/marv
 connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0]) \
     .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
     .groupBy("id").agg(func.sum("connections").alias("connections"))
-    
+
 minConnectionCount = connections.agg(func.min("connections")).first()[0]
 
+#select only rows whose connections equals minconnection count
 minConnections = connections.filter(func.col("connections") == minConnectionCount)
 
+#Join minConnections dataframe and names dataframe on id column
 minConnectionsWithNames = minConnections.join(names, "id")
 
 print("The following characters have only " + str(minConnectionCount) + " connection(s):")
